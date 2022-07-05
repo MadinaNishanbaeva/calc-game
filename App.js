82c62444ec66875d7bc2735106d32be2eea27728
Madina Nishanbaeva <madina_nishanbaeva@epam.com>
2022-07-05 11:57:17 +0500

    init: set up react-native app

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Text>Game...</Text>
+    );
+  }
+}
+
+export default App;
